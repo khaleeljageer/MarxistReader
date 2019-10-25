827e425ef7b5c5053b3519433441c2e76b30e8a5
Khaleel Jageer <jskcse4@gmail.com>
2019-10-25 12:54:38 +0530

    read_percent implementation

--- app/src/main/java/com/marxist/android/database/dao/LocalFeedsDao.kt
+++ app/src/main/java/com/marxist/android/database/dao/LocalFeedsDao.kt
@@ -16,4 +16,7 @@ interface LocalFeedsDao {
 
     @Query("UPDATE localFeeds SET is_bookmarked = :isBookMarked WHERE title = :title AND pub_date = :pubDate")
     fun updateBookMarkStatus(isBookMarked: Boolean, title: String, pubDate: Long)
+
+    @Query("UPDATE localFeeds SET read_percent = :readPercent WHERE title = :title AND pub_date = :pubDate")
+    fun updateReadPercentage(readPercent: Int, title: String, pubDate: Long)
 }
