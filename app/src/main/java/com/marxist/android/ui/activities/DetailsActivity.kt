1da4d93bc6e88c4cc3203a88a3765348b90a5c51
Khaleel Jageer <khaleel@theviralfever.com>
2019-10-25 12:54:38 +0530

    read_percent implementation

--- app/src/main/java/com/marxist/android/ui/activities/DetailsActivity.kt
+++ app/src/main/java/com/marxist/android/ui/activities/DetailsActivity.kt
@@ -3,14 +3,20 @@ package com.marxist.android.ui.activities
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
+import androidx.core.widget.NestedScrollView
 import com.google.android.material.appbar.AppBarLayout
 import com.marxist.android.R
 import com.marxist.android.database.entities.LocalFeeds
 import com.marxist.android.ui.base.BaseActivity
 import com.marxist.android.utils.AppPreference.get
 import kotlinx.android.synthetic.main.activity_details.*
+import kotlin.math.roundToInt
 
 class DetailsActivity : BaseActivity() {
+    private var article: LocalFeeds? = null
+    private var readPercent: Int = 0
+    private var isEndReached: Boolean = false
+
     companion object {
         const val ARTICLE = "article"
     }
@@ -19,9 +25,9 @@ class DetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        val article = intent.getSerializableExtra(ARTICLE) as LocalFeeds
+        article = intent.getSerializableExtra(ARTICLE) as LocalFeeds
 
-        article.title.apply {
+        article!!.title.apply {
             txtCollapseTitle.text = this
             txtFeedTitle.text = this
         }
@@ -29,11 +35,11 @@ class DetailsActivity : BaseActivity() {
         val fontSize = appPreference[getString(R.string.pref_key_font_size), 14]
         txtContent.apply {
             setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize.toFloat())
-            setHtml(article.content)
+            setHtml(article!!.content)
         }
 
-        txtContent.setHtml(article.content)
-        article.isBookMarked.apply {
+        txtContent.setHtml(article!!.content)
+        article!!.isBookMarked.apply {
             ivBookMark.tag = this
             if (this) {
                 ivBookMark.playAnimation()
@@ -51,8 +57,9 @@ class DetailsActivity : BaseActivity() {
             }
 
             ivBookMark.tag = !tag
-            article.isBookMarked = !tag
-            appDatabase.localFeedsDao().updateBookMarkStatus(!tag, article.title, article.pubDate)
+            article!!.isBookMarked = !tag
+            appDatabase.localFeedsDao()
+                .updateBookMarkStatus(!tag, article!!.title, article!!.pubDate)
         }
         appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
             var scrollRange = -1
@@ -69,12 +76,25 @@ class DetailsActivity : BaseActivity() {
             }
         })
 
+        nestedScroll.setOnScrollChangeListener { scrollView: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int ->
+            if (scrollView != null) {
+                val totalHeight =
+                    scrollView.getChildAt(0).measuredHeight - scrollView.measuredHeight
+                isEndReached = scrollY == totalHeight
+                readPercent = (scrollY.toDouble() / totalHeight.toDouble() * 100).roundToInt()
+            }
+        }
+
         ivBack.setOnClickListener {
             onBackPressed()
         }
     }
 
     override fun onBackPressed() {
+        if (article != null) {
+            appDatabase.localFeedsDao()
+                .updateReadPercentage(readPercent, article!!.title, article!!.pubDate)
+        }
         super.onBackPressed()
     }
 
