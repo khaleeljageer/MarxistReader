e2864fcb54d35302b12e87268a9e5b448cd5507d
Khaleel Jageer <jskcse4@gmail.com>
2019-10-30 10:26:01 +0530

    share, copy added

--- app/src/main/java/com/marxist/android/model/RxDataModels.kt
+++ app/src/main/java/com/marxist/android/model/RxDataModels.kt
@@ -1,4 +1,5 @@
 package com.marxist.android.model
 
 data class NetWorkMessage(var message: String, val type: ConnectivityType)
+data class ShowSnackBar(var message: String)
 data class DarkModeChanged(var message: String)
--- app/src/main/java/com/marxist/android/utils/DeviceUtils.kt
+++ app/src/main/java/com/marxist/android/utils/DeviceUtils.kt
@@ -1,7 +1,9 @@
 package com.marxist.android.utils
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
+import com.marxist.android.R
 
 object DeviceUtils {
 
@@ -9,5 +11,23 @@ object DeviceUtils {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork = cm.activeNetworkInfo
         return activeNetwork != null && activeNetwork.isConnected
+    }
+
+    fun shareIntent(
+        subject: String,
+        textExtra: String,
+        baseContext: Context
+    ) {
+        val sendIntent = Intent(Intent.ACTION_SEND)
+        sendIntent.type = "text/plain"
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textExtra)
+
+        val shareIntent = Intent.createChooser(
+            sendIntent,
+            baseContext.getString(R.string.share_text)
+        )
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        baseContext.startActivity(shareIntent)
     }
 }
