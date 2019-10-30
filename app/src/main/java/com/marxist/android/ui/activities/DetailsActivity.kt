3ad5e367197275905ccbf25c59529089f6133ac2
Khaleel Jageer <khaleel@theviralfever.com>
2019-10-30 10:26:01 +0530

    share, copy added

--- app/src/main/java/com/marxist/android/ui/activities/DetailsActivity.kt
+++ app/src/main/java/com/marxist/android/ui/activities/DetailsActivity.kt
@@ -3,21 +3,25 @@ package com.marxist.android.ui.activities
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.TypedValue
-import android.view.View
+import android.view.*
 import androidx.core.widget.NestedScrollView
 import com.google.android.material.appbar.AppBarLayout
 import com.marxist.android.R
 import com.marxist.android.database.entities.LocalFeeds
+import com.marxist.android.model.ShowSnackBar
 import com.marxist.android.ui.base.BaseActivity
 import com.marxist.android.utils.AppPreference.get
+import com.marxist.android.utils.DeviceUtils
 import com.marxist.android.utils.PrintLog
+import com.marxist.android.utils.RxBus
 import kotlinx.android.synthetic.main.activity_details.*
+import org.sufficientlysecure.htmltextview.HtmlTextView
 import kotlin.math.roundToInt
 
 class DetailsActivity : BaseActivity() {
-    private var clipManager: ClipboardManager? = null
     private var article: LocalFeeds? = null
     private var readPercent: Int = 0
 
@@ -25,10 +29,23 @@ class DetailsActivity : BaseActivity() {
         const val ARTICLE = "article"
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_details, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == R.id.menu_share) {
+            DeviceUtils.shareIntent(article!!.title, article!!.link, applicationContext)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
+        setSupportActionBar(toolbar)
         article = intent.getSerializableExtra(ARTICLE) as LocalFeeds
 
         article!!.title.apply {
@@ -39,7 +56,7 @@ class DetailsActivity : BaseActivity() {
         val fontSize = appPreference[getString(R.string.pref_key_font_size), 14]
 
         if (article!!.audioUrl.isEmpty()) {
-            fabAudio.visibility = View.INVISIBLE
+            fabAudio.hide()
         }
 
         var content = article!!.content
@@ -63,27 +80,6 @@ class DetailsActivity : BaseActivity() {
                 ivBookMark.playAnimation()
             }
         }
-
-
-        clipManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipManager!!.addPrimaryClipChangedListener(object :
-            ClipboardManager.OnPrimaryClipChangedListener {
-            override fun onPrimaryClipChanged() {
-                clipManager!!.removePrimaryClipChangedListener(this)
-                val primaryCLip = clipManager!!.primaryClip
-                if (primaryCLip != null) {
-                    val clip = primaryCLip.getItemAt(0).text
-                    PrintLog.debug("Khaleel", "Clip : $clip")
-                    clipManager!!.primaryClip =
-                        ClipData.newPlainText(
-                            "marxist_clip",
-                            clip.toString().plus("\n படிக்க : ${article!!.link}")
-                        )
-                }
-
-                clipManager!!.addPrimaryClipChangedListener(this)
-            }
-        })
 
         initListeners()
     }
@@ -132,6 +128,15 @@ class DetailsActivity : BaseActivity() {
         ivBack.setOnClickListener {
             onBackPressed()
         }
+
+        txtContent.customSelectionActionModeCallback =
+            MarkTextSelectionActionMode(
+                menuInflater,
+                txtContent,
+                applicationContext,
+                article!!.title,
+                article!!.link
+            )
     }
 
     override fun onBackPressed() {
@@ -140,11 +145,98 @@ class DetailsActivity : BaseActivity() {
                 .updateReadPercentage(readPercent, article!!.title, article!!.pubDate)
         }
         super.onBackPressed()
-        clipManager!!.removePrimaryClipChangedListener(null)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         System.gc()
     }
+
+    class MarkTextSelectionActionMode(
+        private val menuInflater: MenuInflater,
+        private val txtContent: HtmlTextView,
+        private val baseContext: Context,
+        private val title: String,
+        private val link: String
+    ) :
+        ActionMode.Callback {
+        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+            if (item != null) {
+                when (item.itemId) {
+                    R.id.menu_share_item -> {
+                        return try {
+                            val selectedText = getSelected()
+                            if (selectedText.isNotEmpty()) {
+                                DeviceUtils.shareIntent(
+                                    title,
+                                    selectedText.plus("\n படிக்க : $link"),
+                                    baseContext
+                                )
+                            }
+                            mode!!.finish()
+                            true
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            false
+                        }
+                    }
+                    R.id.menu_copy_item -> {
+                        return try {
+                            val selectedText = getSelected()
+                            if (selectedText.isNotEmpty()) {
+                                val clipManager =
+                                    baseContext.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                                val clip = ClipData.newPlainText(
+                                    "marxist_reader",
+                                    selectedText.plus("\n படிக்க : $link")
+                                )
+                                clipManager.primaryClip = clip
+                            }
+                            RxBus.publish(ShowSnackBar(baseContext.getString(R.string.text_copied)))
+                            mode!!.finish()
+                            true
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            false
+                        }
+                    }
+                }
+            }
+            return false
+        }
+
+        private fun getSelected(): String {
+            if (txtContent.isFocused) {
+                val textStartIndex = txtContent.selectionStart
+                val textEndIndex = txtContent.selectionEnd
+
+                val min = 0.coerceAtLeast(textStartIndex.coerceAtMost(textEndIndex))
+                val max = 0.coerceAtLeast(textStartIndex.coerceAtLeast(textEndIndex))
+                return txtContent.text.subSequence(min, max).toString().trim { it <= ' ' }
+            }
+            return ""
+        }
+
+        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_share_popup, menu)
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+            if (menu != null) {
+                menu.removeItem(android.R.id.cut)
+                menu.removeItem(android.R.id.copy)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    menu.removeItem(android.R.id.shareText)
+                }
+            }
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode?) {
+            PrintLog.debug("Khaleel", "ActionMode Destroyed")
+        }
+    }
+
+
 }
