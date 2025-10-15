4bf11264d4c896c6c271b3ef889e956e453624fc
Khaleel Jageer <jskcse4@gmail.com>
2025-10-15 11:33:05 +0530

    ui update

--- navigation/src/main/java/com/jskaleel/android/navigation/model/Route.kt
+++ navigation/src/main/java/com/jskaleel/android/navigation/model/Route.kt
@@ -3,4 +3,7 @@ package com.jskaleel.android.navigation.model
 sealed class Route(val name: String) {
     data object Welcome : Route(name = "Route_Welcome")
     data object Main : Route(name = "Route_Main")
+    data object Feed : Route(name = "Route_Feed")
+    data object Books : Route(name = "Route_Books")
+    data object More : Route(name = "Route_More")
 }
